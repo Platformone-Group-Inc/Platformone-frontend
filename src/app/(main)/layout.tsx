30cf63c4f3d1a3795ba799170aa81ea8c7535c39
Sahil Verma <domainsahil007@gmail.com>
2025-02-06 09:28:56 +0530

    ui: blank sidebar added

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,11 +1,26 @@
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+
 import Header from "@/components/dashboard/header";
+import { AppSidebar } from "@/components/app-sidebar";
 
 const DashboardLayout = ({ children }: React.PropsWithChildren) => {
   return (
-    <div>
-      <Header />
-      {children}
-    </div>
+    <SidebarProvider
+      style={
+        {
+          "--sidebar-width": "280px",
+          // Other styles...
+        } as React.CSSProperties
+      }
+    >
+      <AppSidebar />
+      <main className="w-full">
+        <SidebarTrigger className="absolute translate-y-6 -translate-x-4 bg-white z-10 border p-4 rounded-full" />
+
+        <Header />
+        {children}
+      </main>
+    </SidebarProvider>
   );
 };
 
