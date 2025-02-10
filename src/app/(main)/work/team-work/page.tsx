5ecf2a9bf7956dea32dc2e718145e092731785ad
Sahil Verma <domainsahil007@gmail.com>
2025-02-10 23:58:22 +0530

    ui: implement TeamWork page with filters, tab views, and MyWork tab components

--- src/app/(main)/work/team-work/page.tsx
+++ src/app/(main)/work/team-work/page.tsx
@@ -1,5 +1,20 @@
+import Filters from "../my-work/components/filters";
+import MyWorkTab from "../my-work/components/my-work-tab";
+import TabView from "../my-work/components/tab-views";
+
 const TeamWorkPage = () => {
-  return <div>TeamWorkPage</div>;
+  return (
+    <div className="p-6 space-y-3 bg-white rounded-xl w-full">
+      <div className="flex items-center justify-between">
+        <h1 className="font-medium text-xl">Team Work</h1>
+        <div className="flex items-center gap-6">
+          <Filters />
+          <TabView />
+        </div>
+      </div>
+      <MyWorkTab />
+    </div>
+  );
 };
 
 export default TeamWorkPage;
