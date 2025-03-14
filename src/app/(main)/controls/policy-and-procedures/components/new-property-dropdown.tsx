922771ac18fbaf0928a1f41749808135fb148a71
Sahil Verma <domainsahil007@gmail.com>
2025-03-14 12:55:42 +0530

    ui: implement NewPropertyModal and integrate it with NewPropertyDropdown

--- src/app/(main)/controls/policy-and-procedures/components/new-property-dropdown.tsx
+++ src/app/(main)/controls/policy-and-procedures/components/new-property-dropdown.tsx
@@ -10,62 +10,75 @@ import {
 } from "@/components/ui/dropdown-menu";
 
 import { ChevronDownIcon, PlusIcon } from "lucide-react";
+import NewPropertyModal from "./modals/new-property-modal";
+import { useState } from "react";
 
 const NewPropertyDropdown = () => {
+  const [openModal, setOpenModal] = useState(false);
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger
-        asChild
-        // className="outline-none "
-      >
-        <Button
-          variant={"outline"}
-          className="h-auto stroke-secondary data-[state=open]:outline outline-4 outline-primary-600/10 px-3 py-2.5"
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger
+          asChild
+          // className="outline-none "
         >
-          <PlusIcon className="size-5" />
-          New
-          <ChevronDownIcon className="size-5" />
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-56 space-y-2">
-        <DropdownMenuItem asChild>
           <Button
-            variant={"transparent"}
-            className="px-3 w-full justify-normal rounded-lg cursor-pointer"
+            variant={"outline"}
+            className="h-auto stroke-secondary data-[state=open]:outline outline-4 outline-primary-600/10 px-3 py-2.5"
           >
-            Risk
+            <PlusIcon className="size-5" />
+            New
+            <ChevronDownIcon className="size-5" />
           </Button>
-        </DropdownMenuItem>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem asChild>
-          <Button
-            variant={"transparent"}
-            className="px-3 w-full justify-normal rounded-lg cursor-pointer"
-          >
-            Risk
-          </Button>
-        </DropdownMenuItem>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem asChild>
-          <Button
-            variant={"transparent"}
-            className="px-3 w-full justify-normal rounded-lg cursor-pointer"
-          >
-            Risk
-          </Button>
-        </DropdownMenuItem>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem asChild>
-          <Button
-            variant={"transparent"}
-            className="px-3 w-full justify-normal rounded-lg cursor-pointer"
-          >
-            Risk
-          </Button>
-        </DropdownMenuItem>
-        {/* <DropdownMenuSeparator /> */}
-      </DropdownMenuContent>
-    </DropdownMenu>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-56 space-y-2">
+          <DropdownMenuItem asChild>
+            <Button
+              variant={"transparent"}
+              onClick={() => setOpenModal(true)}
+              className="px-3 w-full justify-normal rounded-lg cursor-pointer"
+            >
+              Risk
+            </Button>
+          </DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem asChild>
+            <Button
+              variant={"transparent"}
+              onClick={() => setOpenModal(true)}
+              className="px-3 w-full justify-normal rounded-lg cursor-pointer"
+            >
+              Risk
+            </Button>
+          </DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem asChild>
+            <Button
+              variant={"transparent"}
+              onClick={() => setOpenModal(true)}
+              className="px-3 w-full justify-normal rounded-lg cursor-pointer"
+            >
+              Risk
+            </Button>
+          </DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem asChild>
+            <Button
+              variant={"transparent"}
+              onClick={() => setOpenModal(true)}
+              className="px-3 w-full justify-normal rounded-lg cursor-pointer"
+            >
+              Risk
+            </Button>
+          </DropdownMenuItem>
+          {/* <DropdownMenuSeparator /> */}
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <NewPropertyModal
+        open={openModal}
+        onOpenChange={() => setOpenModal(false)}
+      />
+    </>
   );
 };
 
