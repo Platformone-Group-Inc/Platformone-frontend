6cb203421f7d5d132dda72c6fc0444d3b99ea25b
Sahil Verma <domainsahil007@gmail.com>
2025-03-14 07:32:49 +0530

    ui: add CallToActionButtons component and integrate it into PolicyAndProcedures page

---
+++ src/app/(main)/controls/policy-and-procedures/call-to-action-buttons.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+const CallToActionButtons = () => {
+  return <div className="flex items-center gap-3">call-to-action-buttons</div>;
+};
+
+export default CallToActionButtons;
---
+++ src/app/(main)/controls/policy-and-procedures/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import CallToActionButtons from "./call-to-action-buttons";
+
+const PolicyAndProcedures = () => {
+  return (
+    <div className="p-4">
+      <div className="flex items-center justify-between">
+        <h1 className="font-semibold text-lg">AC 1: Policy and Procedures</h1>
+        <CallToActionButtons />
+      </div>
+    </div>
+  );
+};
+
+export default PolicyAndProcedures;
