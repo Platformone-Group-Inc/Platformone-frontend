db55d87359325856c953d79c88defd5b85818fa9
Sahil Verma <domainsahil007@gmail.com>
2025-03-16 12:47:12 +0530

    ui: refactor DashboardLayout for improved structure and integrate ScrollArea component

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,10 +1,10 @@
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 
-import Header from "@/components/dashboard/header";
 import { AppSidebar } from "@/components/app-sidebar";
-// TODO kuchh to krna hai pta nhi kya
-// import { ScrollArea } from "@/components/ui/scroll-area";
+
+import Header from "@/components/dashboard/header";
 import AiAgentProvider from "@/components/providers/ai-agent-provider";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 const DashboardLayout = ({ children }: React.PropsWithChildren) => {
   return (
@@ -15,15 +15,21 @@ const DashboardLayout = ({ children }: React.PropsWithChildren) => {
           // Other styles...
         } as React.CSSProperties
       }
+      className="w-full"
     >
-      <AppSidebar />
-      <main className="w-full">
-        <SidebarTrigger className="absolute translate-y-6 -translate-x-4 z-50 bg-white border rounded-full" />
-        <Header />
-        <div className="p-8 bg-primary-100 flex min-h-svh gap-4">
-          <AiAgentProvider>{children}</AiAgentProvider>
+      <div className="relative w-full h-screen flex items-stretch">
+        <div className="relative">
+          <AppSidebar />
+          <SidebarTrigger className="absolute top-0 right-0 translate-y-6 translate-x-4 z-50 bg-white border rounded-full" />
         </div>
-      </main>
+
+        <ScrollArea className="rounded-lg bg-primary-100 flex-1 mx-auto overflow-y-auto">
+          <Header />
+          <div className="bg-primary-100 w-full p-4 flex items-stretch gap-4">
+            <AiAgentProvider>{children}</AiAgentProvider>
+          </div>
+        </ScrollArea>
+      </div>
     </SidebarProvider>
   );
 };
