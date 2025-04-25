5484a575666ba82385dbcd6bbbb41461054faf4e
Sahil Verma <domainsahil007@gmail.com>
2025-04-25 12:43:54 +0530

    ui: refactor DashboardLayout to use AppSidebarShell and update NavItem for collapsible functionality

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,40 +1,36 @@
 "use client";
 
 import AiAgent from "@/components/ai-agent";
-
 import { AnimatePresence } from "motion/react";
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 
-import { AppSidebar } from "@/components/app-sidebar";
+import { AppSidebarShell } from "@/components/app-sidebar";
 
 import Header from "@/components/dashboard/header";
 import { useAiChat } from "@/store/useAiChatStore";
 import { ScrollArea } from "@/components/ui/scroll-area";
-import { RadixSidebarDemo } from "@/components/app-sidebar/sidebar-demo";
 
 const DashboardLayout = ({ children }: React.PropsWithChildren) => {
   const { isOpen } = useAiChat();
 
   return (
-    <>
-<div className="rounded-lg bg-primary-100 h-full flex-1 mx-auto ">
-          <Header />
-          <div className="bg-primary-100 w-full min-h-[calc(100vh-5.5rem)] p-4 flex items-stretch gap-4 overflow-y-auto">
-            <AnimatePresence mode="popLayout">
-              <div
-                key={"tab"}
-                className="rounded-xl flex-grow bg-white shadow-md w-full h-full transition-all"
-              >
-                <ScrollArea className="w-full overflow-y-scroll max-h-[calc(100vh-120px)]">
-                  {children}
-                </ScrollArea>
-              </div>
-              {isOpen && <AiAgent key={"ai-agent"} />}
-            </AnimatePresence>
-          </div>
+    <AppSidebarShell>
+      <div className="rounded-lg bg-primary-100 h-full flex-1 mx-auto ">
+        <Header />
+        <div className="bg-primary-100 w-full min-h-[calc(100vh-5.5rem)] p-4 flex items-stretch gap-4 overflow-y-auto">
+          <AnimatePresence mode="popLayout">
+            <div
+              key={"tab"}
+              className="rounded-xl flex-grow bg-white shadow-md w-full h-full transition-all"
+            >
+              <ScrollArea className="w-full overflow-y-scroll max-h-[calc(100vh-120px)]">
+                {children}
+              </ScrollArea>
+            </div>
+            {isOpen && <AiAgent key={"ai-agent"} />}
+          </AnimatePresence>
         </div>
-
-    </>
+      </div>
+    </AppSidebarShell>
   );
 };
 
--- src/components/app-sidebar/nav-item.tsx
+++ src/components/app-sidebar/nav-item.tsx
@@ -2,96 +2,118 @@
 
 import { ChevronRight } from "lucide-react";
 import Link from "next/link";
+import { motion } from "motion/react";
 import { usePathname } from "next/navigation";
 
 import {
+  Collapsible,
   CollapsibleContent,
   CollapsibleTrigger,
 } from "@/components/ui/collapsible";
 
-import {
-  SidebarMenuButton,
-  SidebarMenuItem,
-  SidebarMenuSub,
-  SidebarMenuSubButton,
-  SidebarMenuSubItem,
-} from "@/components/ui/sidebar";
 import { SidebarItem } from "../dashboard/constants/sidebar-data";
 import { cn } from "@/lib/utils";
+import { useEffect, useState } from "react";
 
 interface Props {
   menu: SidebarItem;
+  isCollapsed?: boolean;
+  close?: () => void;
 }
 
-const NavItem: React.FC<Props> = ({ menu }) => {
+const NavItem: React.FC<Props> = ({ menu, isCollapsed, close = () => {} }) => {
   const path = usePathname();
+  const [open, setOpen] = useState(false);
   const hasSubItems = menu.group && menu.group.length > 0;
   const isActive =
     path === menu.href ||
     (hasSubItems && menu.group?.some((subItem) => path === subItem.href));
 
+  useEffect(() => {
+    if (!isCollapsed) {
+      setOpen(false);
+    }
+  }, [isCollapsed]);
+
   return (
-    <SidebarMenuItem>
+    <Collapsible open={open} onOpenChange={setOpen} className="relative">
+      {isActive && (
+        <motion.div
+          layoutId="active-menu"
+          className={cn(
+            "absolute top-0 left-0 rounded-3xl pointer-events-none  p-4",
+            isCollapsed ? "h-full w-full" : "h-full w-auto aspect-square",
+            hasSubItems ? "bg-white/10" : "bg-white"
+          )}
+        />
+      )}
       {hasSubItems ? (
         <>
           <CollapsibleTrigger asChild>
-            <SidebarMenuButton
-              tooltip={menu.label}
+            <button
               className={cn(
-                "rounded-full h-auto py-3 px-4 flex items-center gap-2 text-sm font-medium text-white transition-all group-focus:text-gray-800 stroke-white hover:bg-white/20 hover:text-white hover:stroke-white",
-                isActive && "bg-white/20 text-white"
+                "rounded-full h-auto flex items-center gap-2 text-sm font-medium text-white transition-all group-focus:text-gray-800 stroke-white  w-full",
+                "hover:bg-white/20 hover:text-white hover:stroke-white",
+                isCollapsed ? " py-3 px-4  " : " p-3 aspect-square "
               )}
+              onClick={() => {
+                close();
+                setOpen((s) => !s);
+              }}
             >
               <menu.icon size={20} />
-              <span>{menu.label}</span>
-              <ChevronRight
-                className={cn("ml-auto transition-transform duration-200")}
-              />
-            </SidebarMenuButton>
+              {isCollapsed && (
+                <>
+                  <span>{menu.label}</span>
+                  <ChevronRight
+                    size={15}
+                    className={cn("ml-auto  transition-transform duration-200")}
+                  />
+                </>
+              )}
+            </button>
           </CollapsibleTrigger>
 
-          <CollapsibleContent>
-            <SidebarMenuSub className="space-y-2 py-2 border-white/30">
+          <CollapsibleContent className="ml-10">
+            <div className="space-y-2 py-2 border-white/30">
               {menu?.group?.map((subItem) => {
                 const isSubActive = path === subItem.href;
                 return (
-                  <SidebarMenuSubItem key={subItem.label}>
-                    <SidebarMenuSubButton asChild>
-                      <Link
-                        href={subItem.href as string}
-                        className={cn(
-                          "flex items-center gap-2 text-sm px-2 py-1 transition-all text-primary-200 stroke-primary-200 hover:bg-white/20",
-                          isSubActive && "text-white stroke-white "
-                          //   ? "!text-white"
-                          //   : "text-white hover:text-white"
-                        )}
-                      >
-                        {subItem.icon && <subItem.icon size={18} />}
-                        <span>{subItem.label}</span>
-                      </Link>
-                    </SidebarMenuSubButton>
-                  </SidebarMenuSubItem>
+                  <div key={subItem.label}>
+                    <Link
+                      href={subItem.href as string}
+                      className={cn(
+                        "flex items-center gap-2 text-sm px-2 py-1 transition-all text-primary-200 stroke-primary-200 hover:bg-white/20 rounded-l-md",
+                        isSubActive && "text-white stroke-white bg-white/10 "
+                        //   ? "!text-white"
+                        //   : "text-white hover:text-white"
+                      )}
+                    >
+                      {subItem.icon && <subItem.icon size={18} />}
+                      <span>{subItem.label}</span>
+                    </Link>
+                  </div>
                 );
               })}
-            </SidebarMenuSub>
+            </div>
           </CollapsibleContent>
         </>
       ) : (
-        <Link href={(menu.href as string) || ""} passHref>
-          <SidebarMenuButton
-            tooltip={menu.label}
+        <Link href={(menu.href as string) || ""} className="relative">
+          <button
             className={cn(
-              "rounded-full h-auto py-3 px-4 flex items-center gap-2 text-sm font-medium text-primary-100 transition-all group-focus:text-gray-800 hover:bg-white/20 hover:text-white stroke-white",
+              "rounded-full h-auto flex items-center gap-2 text-sm font-medium text-primary-100 transition-all group-focus:text-gray-800 hover:bg-white/20 hover:text-white stroke-white w-full",
               isActive &&
-                "bg-white hover:bg-white stroke-primary text-primary hover:text-primary"
+                "text-primary stroke-primary hover:text-primary hover:stroke-primary",
+              isCollapsed ? " py-3 px-4  " : " p-3 aspect-square "
             )}
           >
             <menu.icon size={20} />
-            <span>{menu.label}</span>
-          </SidebarMenuButton>
+            {isCollapsed && <span>{menu.label}</span>}
+          </button>
         </Link>
       )}
-    </SidebarMenuItem>
+    </Collapsible>
   );
 };
 
