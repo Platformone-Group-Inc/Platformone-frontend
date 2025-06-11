41cf15adf20a84f6fb7b4e038a040e0450902dcc
Sahil Verma <domainsahil007@gmail.com>
2025-06-11 13:19:23 +0530

    fix: update grid layout in AuthLayout to use lg breakpoint for better responsiveness

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 const AuthLayout = ({ children }: React.PropsWithChildren) => {
   return (
-    <div className="w-full h-dvh overflow-hidden grid grid-cols-1 md:grid-cols-2">
+    <div className="w-full h-dvh overflow-hidden grid grid-cols-1 lg:grid-cols-2">
       <div
         className="hidden relative bg-primary bg-no-repeat bg-cover h-full w-full lg:flex flex-col gap-6 items-center justify-center rounded-br-[117px] text-white text-center"
         style={{
@@ -14,14 +14,15 @@ const AuthLayout = ({ children }: React.PropsWithChildren) => {
         <AnimatedContainer animation="fade" isOpen>
           <Image src="/images/logo.svg" alt="logo" height={150} width={180} />
         </AnimatedContainer>
-        <p className="text-lg max-w-xl">
+        <AnimatedContainer delay={0.2} isOpen className="text-lg max-w-xl">
           Your single source for streamlined FedRAMP and CMMC compliance. Gain
           confidence in your security posture, ensure regulatory alignment, and
           drive operational excellence—all in one integrated platform.
           <br />
-          <br />
+        </AnimatedContainer>
+        <AnimatedContainer delay={0.3} isOpen className="text-lg max-w-xl">
           Login to simplify compliance and safeguard your organization today.
-        </p>
+        </AnimatedContainer>
       </div>
 
       <ScrollArea className="w-full h-full overflow-y-auto flex flex-col items-center justify-center">
