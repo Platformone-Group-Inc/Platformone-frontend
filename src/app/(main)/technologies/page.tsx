a0a1af893c3675101b2011c50826c65192c4475f
Sahil Verma <domainsahil007@gmail.com>
2025-06-18 19:57:51 +0530

    refactor: clean up code formatting and improve readability in TechnologiesPage

--- src/app/(main)/technologies/page.tsx
+++ src/app/(main)/technologies/page.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 "use client";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { InfoCircle } from "iconsax-react";
