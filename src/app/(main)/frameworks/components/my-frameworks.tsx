b2e38b46dab16d16575f237cf3b6a933b9e0edcb
Mayureshju <juvekarmayuresh37@gmail.com>
2025-06-20 19:39:25 +0530

    feat: refactor MyFrameworks component to improve loading state handling and structure

--- src/app/(main)/frameworks/components/my-frameworks.tsx
+++ src/app/(main)/frameworks/components/my-frameworks.tsx
@@ -9,6 +9,7 @@ import FrameworksCard from "./frameworks-card";
 import { useQuery } from "@tanstack/react-query";
 import { getFrameworksByOrganizationQueryFn } from "@/services/operations/Framework";
 import { useAuthContext } from "@/context/auth-provider";
+import NoFrameworks from "./no-frameworks";
 const MyFrameworks = () => {
   const { user, isLoading: authLoading } = useAuthContext();
 
@@ -21,34 +22,51 @@ const MyFrameworks = () => {
     queryFn: () => getFrameworksByOrganizationQueryFn(user?.organization),
     enabled: !!user?.organization
   });
-  return (
-    <div className="space-y-6">
-      <div className="border-b pb-6 flex items-center justify-between">
-        <div className="space-y-1">
-          <h1 className="font-semibold inline-flex items-center gap-2 text-xl">
-            My Frameworks
-            <InfoIcon className="" size={16} />
-            <Badge className="rounded-md" variant={"secondary"}>
-              {myFrameworks?.frameworks?.length} Framework
-            </Badge>
-          </h1>
 
-          <p className="text-sm text-secondary-400">
-            Keep track of vendor and their security ratings.
-          </p>
-        </div>
-        <Link href={"/frameworks/new"} className={cn(buttonVariants())}>
-          <PlusIcon size={20} />
-          Add Framework
-        </Link>
-      </div>
-      <div className="grid gap-6 grid-cols-3">
-         {myFrameworks?.frameworks?.map((framework: any) => (
-          <FrameworksCard key={framework?._id} framework={framework} />
-        ))}
-       
-      </div>
-    </div>
+
+  if (myFrameworksLoading) {
+    return <p> Loading Frameworks</p>
+  }
+  return (
+    <>
+      {
+        myFrameworks?.frameworks?.length > 0 && (
+          <div className="space-y-6">
+            <div className="border-b pb-6 flex items-center justify-between">
+              <div className="space-y-1">
+                <h1 className="font-semibold inline-flex items-center gap-2 text-xl">
+                  My Frameworks
+                  <InfoIcon className="" size={16} />
+                  <Badge className="rounded-md" variant={"secondary"}>
+                    {myFrameworks?.frameworks?.length} Framework
+                  </Badge>
+                </h1>
+
+                <p className="text-sm text-secondary-400">
+                  Keep track of vendor and their security ratings.
+                </p>
+              </div>
+              <Link href={"/frameworks/new"} className={cn(buttonVariants())}>
+                <PlusIcon size={20} />
+                Add Framework
+              </Link>
+            </div>
+            <div className="grid gap-6 grid-cols-3">
+              {myFrameworks?.frameworks?.map((framework: any) => (
+                <FrameworksCard key={framework?._id} framework={framework} />
+              ))}
+
+            </div>
+          </div>
+
+        )
+
+
+      }
+      {
+        myFrameworks?.frameworks?.length === 0 && <NoFrameworks />
+      }
+    </>
   );
 };
 
