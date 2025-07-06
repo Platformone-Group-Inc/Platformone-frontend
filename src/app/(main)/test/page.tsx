57066ec892d1ed16c5f7b4d63d1c30a7e618dbed
Sahil Verma <domainsahil007@gmail.com>
2025-07-06 17:25:48 +0530

    fix: build issue fixed

--- src/app/(main)/test/page.tsx
+++ src/app/(main)/test/page.tsx
@@ -1,28 +1,35 @@
-"use client";
+// "use client";
 
-import RadialChart from "@/components/charts/radial-chart";
-import { useState, useEffect } from "react";
-import MyWorkTable from "../work/my-work/_components/tables/my-work-data-table";
+// import RadialChart from "@/components/charts/radial-chart";
+// import { useState, useEffect } from "react";
+// import MyWorkTable from "../work/my-work/_components/tables/my-work-data-table";
 
-const TestPage = () => {
-  const [value, setValue] = useState(0);
+// const TestPage = () => {
+//   // const [value, setValue] = useState(0);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setValue(Math.floor(Math.random() * 101));
-    }, 1000);
+//   useEffect(() => {
+//     const interval = setInterval(() => {
+//       setValue(Math.floor(Math.random() * 101));
+//     }, 1000);
 
-    return () => clearInterval(interval);
-  }, []);
-  return (
-    <div>
-      {/* <RadialChart value={value} />
-      <RadialChart value={85} size={120} />
-      <RadialChart value={45} size={120} />
-      <RadialChart value={15} size={120} /> */}
-      <MyWorkTable />
-    </div>
-  );
+//     return () => clearInterval(interval);
+//   }, []);
+//   return (
+//     <div>
+//       {/* <RadialChart value={value} />
+//       <RadialChart value={85} size={120} />
+//       <RadialChart value={45} size={120} />
+//       <RadialChart value={15} size={120} /> */}
+//       {/* <MyWorkTable /> */}
+//     </div>
+//   );
+// };
+
+// export default TestPage;
+import React from "react";
+
+const Test = () => {
+  return <div>Test</div>;
 };
 
-export default TestPage;
+export default Test;
